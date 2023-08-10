f479a39d71c8c3c06114eebdd025f3ad1d871f2c
Misha <shevchenkom606@gmail.com>
2023-08-10 11:44:36 +0200

    delate array

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,42 +2,15 @@ import PropTypes from "prop-types";
 import css from './Statistics.module.css'
 
 export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
-    const statisticArray = [
-        { statItem: good, title: 'Good' },
-        { statItem: neutral, title: 'Neutral' },
-        { statItem: bad, title: 'Bad' },
-        { statItem: total, title: 'Total feedbacks' },
-        { statItem: positivePercentage, title: 'Positive feedbacks' },
-    ];
 
     return (
         <ul className={css.statisticList}>
-            {statisticArray.slice(0, -2).map(({statItem, title}) => (
-            <li className={css.statisticItem} key={title}>
-                <p className={css.statisticP}>
-                    {title}:{' '}
-                    <span className={css.statisticSpan}>
-                    {title !== 'Positive feedbacks'
-                ? statItem : positivePercentage + '%'}
-                    </span>
-                </p>
-            </li>
-            ))}
-
-            <div className={css.borderContainer}>
-                {statisticArray.slice(-2).map(({ statItem, title }) => ( 
-                <li className={css.statisticItemL} key={title}>
-                    <p className={css.statisticP}>
-                    {title}:{' '}
-                    <span className={css.statisticSpan}>
-                    {title !== 'Positive feedbacks' ? statItem : positivePercentage + '%'}
-                    </span>
-                    </p>
-                </li>
-                ))}
-            </div>
+            <li>Good: {good} </li>
+            <li>Neutral: {neutral}</li>
+            <li>Bad: {bad}</li>
+            <li>Total: {total}</li>
+            <li>Positive feedbacks: {positivePercentage}%</li>
         </ul>
-
     );
 };
 Statistics.propTypes = {
